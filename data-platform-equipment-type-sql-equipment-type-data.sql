75ee7f5bdb0e24a207b97d08e6f968babb8f00d0
maimeimow <134258119+maimeimow@users.noreply.github.com>
2023-08-26 15:00:06 +0900

    release

--- data-platform-equipment-type-sql-equipment-type-data.sql
+++ data-platform-equipment-type-sql-equipment-type-data.sql
@@ -1,6 +1,9 @@
 CREATE TABLE `data_platform_equipment_type_equipment_type_data`
 (
-    `EquipmentType`        varchar(10) NOT NULL,
+    `EquipmentType`          varchar(10) NOT NULL,
+    `CreationDate`           date NOT NULL,
+    `LastChangeDate`         date NOT NULL,
+    `IsMarkedForDeletion`    tinyint(1) DEFAULT NULL,
     
     PRIMARY KEY (`EquipmentType`)
 
--- data-platform-equipment-type-sql-equipment-type-text-data.sql
+++ data-platform-equipment-type-sql-equipment-type-text-data.sql
@@ -1,12 +1,16 @@
 CREATE TABLE `data_platform_equipment_type_equipment_type_text_data`
 (
     `EquipmentType`                varchar(10) NOT NULL,
-    `Language`                     varchar(3) NOT NULL,
-    `EquipmentTypeName`            varchar(100) NOT NULL,
+    `Language`                     varchar(2) NOT NULL,
+    `EquipmentTypeName`            varchar(200) NOT NULL,
+    `CreationDate`                 date NOT NULL,
+    `LastChangeDate`               date NOT NULL,
+    `IsMarkedForDeletion`          tinyint(1) DEFAULT NULL,
 
     PRIMARY KEY (`EquipmentType`, `Language`),
 
-    CONSTRAINT `DPFMEquipmentTypeTextData_fk` FOREIGN KEY (`EquipmentType`) REFERENCES `data_platform_equipment_type_equipment_type_data` (`EquipmentType`)
+    CONSTRAINT `DPFMEquipmentTypeTextData_fk` FOREIGN KEY (`EquipmentType`) REFERENCES `data_platform_equipment_type_equipment_type_data` (`EquipmentType`),
+    CONSTRAINT `DPFMEquipmentTypeTextDataLanguage_fk` FOREIGN KEY (`Language`) REFERENCES `data_platform_language_language_data` (`Language`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
